5ced73aa6659c526944532f97ac4ebe8bc813a3b
moto4321 <moto432195@gmail.com>
2022-08-03 14:56:17 +0900

    [feat] 직방 찜한 원룸 정보 크롤링 성공

--- server/routes/Auth.js
+++ server/routes/Auth.js
@@ -39,11 +39,8 @@ router.post("/registration", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body
-
   const user = await Users.findOne({ where: { email: email } })
 
-  // console.log(user.email)
-  // console.log(user.password)
   if (!user) {
     res.json({ error: "This email doesn't exist" })
   } else {
@@ -79,18 +76,26 @@ router.post("/zigbang/login", async (req, res) => {
     // 직방 카카오 로그인 전 페이지
     await page.goto(zigbangKakaoUrl)
     // 페이지가 전부 로드된 후 작업실행
-    console.log(1)
-    // await page.waitForNavigation();
     // 카카오 로그인 버튼 클릭
-    console.log(99)
     await page.click('#kakao_로그인 > div.css-901oao.r-1b277g5.r-143r1dj.r-ubezar')
     // 페이지가 전부 로드된 후 작업실행
-    console.log(2)
-    // await page.waitForNavigation();
     await page.waitForNavigation({ waitUntil: 'networkidle0' })
 
     if (page.url() === 'https://account.zigbang.com/v2/oauth/login?provider=kakao') {
       await page.goto(zigbangUrl)
+      await page.waitForNavigation({ waitUntil: 'networkidle0' })
+      console.log(page.url())
+      await page.click('.i_account')
+      await page.waitForNavigation({ waitUntil: 'networkidle0' })
+      console.log(page.url())
+      await page.click('#kakao_로그인')
+      await page.waitForSelector('#id_email_2')
+      console.log('aaa', page.url())
+      // 이메일 패스워드 input값 입력
+      await page.type( "#id_email_2", email);
+      await page.type( "#id_password_3", password);
+      // 로그인 버튼 클릭
+      await page.click('.submit')
     } else {
       // 이메일 패스워드 input값 입력
       await page.type( "#id_email_2", email);
@@ -99,68 +104,18 @@ router.post("/zigbang/login", async (req, res) => {
       await page.click('.submit')
     }
 
-    // 내가 북마크한 정보들 크롤링
+    page.waitForNavigation()
+
+    await page.waitForTimeout(30000)
     await page.goto(`https://www.zigbang.com/item/zzim`)
-    const selector = "body > div.wrap > div.container > div > div.content.left-content > div > div.zzim-list > a:nth-child(2) > div > div.i-tit > strong"
+
+    // 내가 북마크한 정보들 크롤링
+    const selector = 'body > div.wrap > div.container > div > div.content.left-content > div > div.zzim-list'
     await page.waitForSelector(selector)
     const data = await page.$eval(selector, (elem) => elem.textContent)
 
     console.log(data)
   })
-
-  //
-
-
-
-
-  // const browser = await puppeteer.launch()
-  // const page = await browser.newPage()
-
-  
-  // // 페이지가 전부 로드된 후 작업실행
-  // console.log(1)
-  // await page.waitForNavigation();
-  // // 카카오 로그인 버튼 클릭
-  // console.log(99)
-  // await page.click('#kakao_로그인 > div.css-901oao.r-1b277g5.r-143r1dj.r-ubezar')
-  // // 페이지가 전부 로드된 후 작업실행
-  // console.log(2)
-  // await page.waitForNavigation();
-  // // 이메일 패스워드 input값 입력
-	// await page.type( "#id_email_2", email);
-	// await page.type( "#id_password_3", password);
-  // // // 로그인 버튼 클릭
-  // await page.click('.submit')
-  // // 페이지가 전부 로드된 후 작업실행
-  // console.log(3)
-  // await page.waitForNavigation();
-
-  // // 내가 북마크한 정보들 크롤링
-  // await page.goto(`https://www.zigbang.com/item/zzim`)
-  // const selector = "body > div.wrap > div.container > div > div.content.left-content > div > div.zzim-list > a:nth-child(2) > div > div.i-tit > strong"
-  // await page.waitForSelector(selector)
-  // const data = await page.$eval(selector, (elem) => elem.textContent)
-
-  // console.log(data)
-  // if (page.url() === zigbangLoginUrl) {
-  //   // 로그인 실패
-  // } else {
-  //   // 내가 북마크한 정보들 크롤링
-  //   await page.goto(`https://www.zigbang.com/item/zzim`)
-  //   // body > div.wrap > div.container > div > div.content.left-content > div > div.zzim-list
-  //   const selector = "body > div.wrap > div.container > div > div.content.left-content > div > div.zzim-list > a:nth-child(2) > div > div.i-tit > strong"
-  //   await page.waitForSelector(selector)
-
-  //   const data = await page.$eval(selector, (elem) => elem.textContent)
-
-  //   console.log(data)
-
-    // 홈으로 redirect
-  // }
-
-  
-
-
 })
 
 module.exports = router
